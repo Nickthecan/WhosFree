98ea443240e51c9139b48b96f1900bb5ce7856f4
Nicholas Amancio <nick.amancio44@gmail.com>
2025-08-02 21:54:15 -0700

    working on landing page

--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -1,9 +1,16 @@
 import { Link } from "react-router-dom";
+import calendarPicture from "../assets/CalendarPicture.png"
+import "../styles/pages/Landing.css"
 
 const Landing = () => {
     return (
         <>
-            
+            <div className="landing-body">
+                <h1 className="landing-title lato-bold">Who's Free</h1>
+                <p className="landing-subtitle lato-bold">Smooth Planning for Everyone!</p>
+                <button  className="landing-button"><p className="lato-bold">Start Planning</p></button>
+                <img className="landing-calendar-picture" src={calendarPicture} alt="" />
+            </div>
         </>
     )
 }
