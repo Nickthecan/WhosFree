ba37cdc772ec2f8f0442da57c09f547cbdfc8d0c
Nicholas Amancio <112116021+Nickthecan@users.noreply.github.com>
2025-11-21 07:14:33 -0800

    Navbar (#5)
    * added photos, added css
    
    * fixed up css issues
    
    * navbar problems
    
    * updated calendar component
    
    * undid testing code for navbar
    
    ---------
    
    Co-authored-by: Nicholas Amancio <508804@cds.ci.la.ca.us>

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,28 +1,52 @@
 import "../styles/components/Navbar.css"
 import { Link } from "react-router-dom"
 import Hambuger from "../assets/hamburger.png"
+import close from "../assets/close.png"
+import planImage from "../assets/planImage.svg"
+import profileImage from "../assets/profileImage.svg"
+import eventsImage from "../assets/eventsImage.svg"
 import { useState } from "react"
 
 const Navbar = () => {
     const [navbarMobile, setNavbarMobile] =  useState(false)
+    const [navbarMobilePreview, setNavbarMobilePreview] = useState(true)
 
     const activateNavBar = () => {
         setNavbarMobile(!navbarMobile)
+        setNavbarMobilePreview(!navbarMobilePreview)
     }
 
     return (
         <>
-            <div className="c-navbar-row">
-                <img className="c-nav-bar-burger" src={Hambuger} alt="" onClick={activateNavBar}/>
-                <h1 className="c-website-name">Who's Free</h1>
+            <div className={navbarMobilePreview ? `c-navbar-row` : `c-navbar-row-hidden`}>
+                <div className="c-navbar-row-items">
+                    <img className="c-nav-bar-burger" src={Hambuger} alt="" onClick={activateNavBar}/>
+                    <h1 className="c-website-name">Who's Free</h1>
+                </div>
             </div>
             
             {navbarMobile && (
                 <div className="c-navbar-body-mobile">
-                    <Link to={"/home"}><h1>Home</h1></Link>
-                    <Link to={"/plan"}><h1>Plan</h1></Link>
-                    <Link to={"/events"}><h1>Events</h1></Link>
-                    <Link to={"/account"}><h1>Account</h1></Link>
+                    <div className="c-navbar-row">
+                        <div className="c-navbar-row-items">
+                            <img className="c-nav-bar-close" src={close} alt="" onClick={activateNavBar}/>
+                            <h1 className="c-website-name-navbar-open">Who's Free</h1>
+                        </div>
+                    </div>
+                    <div className="c-navbar-body-mobile-links">
+                        <div className="c-navbar-body-mobile-links-pair">
+                            <img src={planImage} alt="" height="32px"/>
+                            <Link to={"/plan"}><h1>Plan</h1></Link>
+                        </div>
+                        <div className="c-navbar-body-mobile-links-pair">
+                            <img src={eventsImage} alt="" height="32px"/>
+                            <Link to={"/events"}><h1>Events</h1></Link>
+                        </div>
+                        <div className="c-navbar-body-mobile-links-pair">
+                            <img src={profileImage} alt="" height="32px"/>
+                            <Link to={"/account"}><h1>Account</h1></Link>
+                        </div>
+                    </div>
                 </div>
             )}
             
