90cd6c1097037454546ff03a1967fcdb28533554
Nicholas Amancio <nick.amancio44@gmail.com>
2025-11-30 21:38:47 -0800

    css for acocunt

--- client/src/pages/Plan.tsx
+++ client/src/pages/Plan.tsx
@@ -7,7 +7,12 @@ import SelectTime from "../components/SelectTime"
 import 'react-calendar/dist/Calendar.css';
 import '../styles/components/Calendar.css'
 
+import { useState } from "react"
+
 const Plan = () => {
+    const [range, setRange] = useState<[Date | null, Date | null] | null>(null)
+
+
     return (
         <>
             <Navbar />
@@ -15,7 +20,7 @@ const Plan = () => {
             <h2 className="plan-name">Name</h2><input className="plan-input-name" type="text" placeholder="Hangout"/>
             <h2 className="plan-calendar-title">Days Available to Meet</h2>
             {/* <DatePicker /> */}
-            <Calendar className={"c-calendar"}/>
+            <Calendar className={"c-calendar"} selectRange={true} onChange={setRange} value={range}/>
             <h2 className="plan-calendar-title">Times</h2>
             <div className="plan-time-selection">
                 <p className="plan-calendar-subtitle">No earlier than</p>
