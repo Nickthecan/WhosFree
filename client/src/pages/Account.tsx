f3e7dc5bb9c01642bd82b192e2c37941760fa0a9
Nickthecan <nick.amancio44@gmail.com>
2025-12-02 15:24:04 -0800

    fixed the styling for the account page

--- client/src/pages/Account.tsx
+++ client/src/pages/Account.tsx
@@ -7,39 +7,43 @@ const Account = () => {
         <>
             <Navbar />
             <h1 className="title">Account</h1>
-            <div className="a-list">
-                <div className='a-edit-account'>
-                    <div>
-                        <h2>Name</h2>
-                        <button>edit</button>
+            <div className="a-profile-info">
+                <div className="a-list">
+                    <div className='a-edit-account'>
+                        <div className='a-edits'>
+                            <h2>Name</h2>
+                            <button>edit</button>
+                        </div>
+                        <h2 className='a-template'>template name</h2>
                     </div>
-                    <h2 className='a-template'>template name</h2>
-                </div>
-                <div className='a-edit-account'>
-                    <div>
-                        <h2>Email</h2>
-                        <button>edit</button>
+                    <div className='a-edit-account'>
+                        <div className='a-edits'>
+                            <h2>Email</h2>
+                            <button>edit</button>
+                        </div>
+                        <h2 className='a-template'>template email</h2>
                     </div>
-                    <h2 className='a-template'>template email</h2>
-                </div>
-                <div className='a-edit-account'>
-                    <div>
-                        <h2>Password</h2>
-                        <button className='a-small-text-button'>Change Password</button>
+                    <div className='a-edit-account'>
+                        <div className='a-edits'>
+                            <h2>Password</h2>
+                            <button className='a-small-text-button'>Change Password</button>
+                        </div>
+                    </div>
+                    <div className='a-edit-account'>
+                        <div className='a-edits'>
+                            <h2>Theme</h2>
+                            <button>Light</button>
+                        </div>
                     </div>
                 </div>
-                <div className='a-edit-account'>
-                    <div>
-                        <h2>Theme</h2>
-                        <button>Light</button>
+                <div className="a-profile-footer">
+                    <div className='a-edit-account-button'>
+                        <SubmitButton text='Log out'/>
+                    </div>
+                    <div className='a-edit-account-button'>
+                        <SubmitButton text='Delete Account'/>
                     </div>
                 </div>
-            </div>
-            <div className='a-edit-account-button'>
-                <SubmitButton text='Log out'/>
-            </div>
-            <div className='a-edit-account-button'>
-                <SubmitButton text='Delete Account'/>
             </div>
         </>
     )
